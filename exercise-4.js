a6d9aa676d7132ac0b3cb47ee382e7997539267a
ribkaharyuni <40658854+ribkaharyuni@users.noreply.github.com>
2018-07-09 10:27:23 +0700

    switch case

---
+++ exercise-4.js
@@ -0,0 +1,42 @@
+var tanggal = 21; 
+var bulan = 1; 
+var tahun = 1945;
+
+switch(bulan){
+	case 1:
+		console.log(tanggal + " January " + tahun);
+		break;
+	case 2:
+		console.log(tanggal + " February " + tahun);
+		break;
+	case 3:
+		console.log(tanggal + " March " + tahun);
+		break;
+  case 4:
+		console.log(tanggal + " April " + tahun);
+		break;
+  case 5:
+		console.log(tanggal + " May " + tahun);
+		break;
+  case 6:
+		console.log(tanggal + " June " + tahun);
+		break;
+  case 7:
+		console.log(tanggal + " July " + tahun);
+		break;
+  case 8:
+		console.log(tanggal + " August " + tahun);
+		break;
+  case 9:
+		console.log(tanggal + " September " + tahun);
+		break;
+  case 10:
+		console.log(tanggal + " October " + tahun);
+		break;
+  case 11:
+		console.log(tanggal + " November " + tahun);
+		break;
+	default:
+	  console.log(tanggal + " December " + tahun);
+		break;
+}
